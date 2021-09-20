b4f6747f81917dedb8454962acaaf48489f0d954
JWGitonga <jannetwanjiku9@gmail.com>
2021-09-20 21:39:10 +0300

    added intermediate activity

---
+++ app/src/main/java/com/example/diabeteshealthmonitoringapplication/activities/IntermediateActivity.java
@@ -0,0 +1,16 @@
+package com.example.diabeteshealthmonitoringapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.diabeteshealthmonitoringapplication.R;
+
+public class IntermediateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inter_mediatectivity);
+    }
+}
