61f2dba86831752365c6cf219521599b2ac7f42f
James Omondi <jamesomondi1418@gmail.com>
2021-10-19 19:12:32 +0300

    viewmodel for messages

--- app/src/main/java/com/example/diabeteshealthmonitoringapplication/fragments/MessagingActivity.java
+++ app/src/main/java/com/example/diabeteshealthmonitoringapplication/fragments/MessagingActivity.java
@@ -12,11 +12,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.diabeteshealthmonitoringapplication.R;
 import com.example.diabeteshealthmonitoringapplication.adapters.ChatAdapter;
 import com.example.diabeteshealthmonitoringapplication.models.Chat;
+import com.example.diabeteshealthmonitoringapplication.viewmodels.MessagesViewModel;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -33,6 +35,7 @@ public class MessagingActivity extends AppCompatActivity {
     private TextView noChatsYetTv;
     private ImageView noChatsYetIv;
     private EditText messageEt;
+    private MessagesViewModel messagesViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,42 +47,27 @@ public class MessagingActivity extends AppCompatActivity {
         noChatsYetTv = findViewById(R.id.no_chats_yet_tv);
         noChatsYetIv = findViewById(R.id.no_chats_yet_iv);
         messageEt = findViewById(R.id.message_et);
+        messagesViewModel = new ViewModelProvider(this).get(MessagesViewModel.class);
         ImageView send = findViewById(R.id.send);
         List<Chat> chatList = new ArrayList<>();
-        String myId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                FirebaseDatabase.getInstance().getReference("messages")
-                .addValueEventListener(new ValueEventListener() {
-                    @SuppressLint("NotifyDataSetChanged")
-                    @RequiresApi(api = Build.VERSION_CODES.N)
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        snapshot.getChildren().forEach(chat -> {
-                            Chat ch = chat.getValue(Chat.class);
-                            if (ch != null) {
-                                if (ch.getFromUid()==myId && ch.getToUid()==uid || ch.getFromUid()==uid && ch.getToUid()==myId) {
-                                    chatList.add(ch);
-                                }
-                            }
-                        });
-                        if (chatList.isEmpty()) {
-                            noChatsYetIv.setVisibility(View.VISIBLE);
-                            noChatsYetTv.setVisibility(View.VISIBLE);
-                            chatsRecycler.setVisibility(View.GONE);
-                        } else {
-                            noChatsYetIv.setVisibility(View.GONE);
-                            noChatsYetTv.setVisibility(View.GONE);
-                            chatsRecycler.setVisibility(View.VISIBLE);
-                            ChatAdapter adapter = new ChatAdapter(MessagingActivity.this, chatList);
-                            chatsRecycler.setAdapter(adapter);
-                            adapter.notifyDataSetChanged();
-                        }
-                    }
-git
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.e(TAG, "onCancelled: error -> " + error.getMessage());
-                    }
-                });
+        String myId = FirebaseAuth.getInstance().getUid();
+        assert myId != null;
+        messagesViewModel.getMessages(myId,uid).observe(this, chats -> {
+                  if (chats.isEmpty()) {
+                      noChatsYetIv.setVisibility(View.VISIBLE);
+                      noChatsYetTv.setVisibility(View.VISIBLE);
+                      chatsRecycler.setVisibility(View.GONE);
+                  } else {
+                      noChatsYetIv.setVisibility(View.GONE);
+                      noChatsYetTv.setVisibility(View.GONE);
+                      chatsRecycler.setVisibility(View.VISIBLE);
+                      ChatAdapter adapter = new ChatAdapter(MessagingActivity.this, chatList);
+                      chatsRecycler.setAdapter(adapter);
+                      adapter.notifyDataSetChanged();
+                  }
+              });
+
+
         send.setOnClickListener(view -> {
             String message = messageEt.getText().toString();
             Chat chat = new Chat(FirebaseAuth.getInstance().getUid(), uid,message,System.currentTimeMillis());
