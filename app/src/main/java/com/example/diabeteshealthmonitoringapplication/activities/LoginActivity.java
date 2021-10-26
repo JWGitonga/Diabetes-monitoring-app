7c6d48702b12e9dbb6e55afb3ca813a8ff28a9fd
James Omondi <jamesomondi1418@gmail.com>
2021-10-26 18:35:16 +0300

    reset link

--- app/src/main/java/com/example/diabeteshealthmonitoringapplication/activities/LoginActivity.java
+++ app/src/main/java/com/example/diabeteshealthmonitoringapplication/activities/LoginActivity.java
@@ -27,81 +27,78 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Login");
-//        if (FirebaseAuth.getInstance().getUid() != null) {
-//            startActivity(new Intent(this, IntermediateActivity.class));
-//            finish();
-//        } else {
-            progressDialog = new ProgressDialog(this);
-            progressDialog.setCancelable(false);
-            progressDialog.setMessage("Signing in...");
-            EditText mEmail = findViewById(R.id.email_login);
-            EditText pass = findViewById(R.id.password);
-            Button registration = findViewById(R.id.Register);
-            TextView forgotPassword = findViewById(R.id.forgot_password);
-
-            Button login = findViewById(R.id.Login);
-            login.setOnClickListener(v -> {
-                progressDialog.show();
-                this.email = mEmail.getText().toString().trim();
-                this.password = pass.getText().toString().trim();
-                if (email.isEmpty()) {
-                    mEmail.setError("Cannot be empty");
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Signing in...");
+        EditText mEmail = findViewById(R.id.email_login);
+        EditText pass = findViewById(R.id.password);
+        Button registration = findViewById(R.id.Register);
+        TextView forgotPassword = findViewById(R.id.forgot_password);
+        Button login = findViewById(R.id.Login);
+        login.setOnClickListener(v -> {
+            progressDialog.show();
+            this.email = mEmail.getText().toString().trim();
+            this.password = pass.getText().toString().trim();
+            if (email.isEmpty()) {
+                mEmail.setError("Cannot be empty");
+                progressDialog.dismiss();
+            } else {
+                if (password.isEmpty()) {
+                    pass.setError("Cannot be empty");
                     progressDialog.dismiss();
                 } else {
-                    if (password.isEmpty()) {
-                        pass.setError("Cannot be empty");
+                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                        mEmail.setError("Invalid email address");
                         progressDialog.dismiss();
                     } else {
-                        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                            mEmail.setError("Invalid email address");
-                            progressDialog.dismiss();
-                        } else {
-                            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                                    .addOnCompleteListener(task -> {
-                                        if (task.isSuccessful()) {
-                                            progressDialog.dismiss();
-                                            startActivity(new Intent(LoginActivity.this, IntermediateActivity.class));
-                                            finish();
-                                        }
-                                    }).addOnFailureListener(e -> {
-                                if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                                    progressDialog.dismiss();
-                                    Toast.makeText(LoginActivity.this, "Invalid credential", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                        }
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+                                        progressDialog.dismiss();
+                                        startActivity(new Intent(LoginActivity.this, IntermediateActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(e -> {
+                            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivity.this, "Invalid credential", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            }
+        });
+        forgotPassword.setOnClickListener(v -> {
+            EditText email = new EditText(this);
+            email.setMaxEms(10);
+            email.setPadding(50, 20, 50, 20);
+            email.setHint("Enter email address");
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Password recovery");
+            alertDialog.setView(email);
+            alertDialog.setPositiveButton("Submit", (dialog, which) -> {
+                String strEmail = email.getText().toString().trim();
+                if (strEmail.isEmpty()) {
+                    email.setError("Cannot be empty");
+                } else {
+                    if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+                        Toast.makeText(this, "Invalid email address", Toast.LENGTH_SHORT).show();
+                    } else {
+                        FirebaseAuth.getInstance().sendPasswordResetEmail(strEmail).addOnCompleteListener(task -> {
+                            if (task.isSuccessful() && task.isComplete()){
+                                Toast.makeText(this, "Reset link sent to email", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                 }
             });
-            forgotPassword.setOnClickListener(v -> {
-                EditText email = new EditText(this);
-                email.setMaxEms(10);
-                email.setPadding(50, 20, 50, 20);
-                email.setHint("Enter email address");
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-                alertDialog.setTitle("Password recovery");
-                alertDialog.setView(email);
-                alertDialog.setPositiveButton("Submit", (dialog, which) -> {
-                    String strEmail = email.getText().toString().trim();
-                    if (strEmail.isEmpty()) {
-                        email.setError("Cannot be empty");
-                    } else {
-                        if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
-                            Toast.makeText(this, "Invalid email address", Toast.LENGTH_SHORT).show();
-                        } else {
-                            FirebaseAuth.getInstance().sendPasswordResetEmail(strEmail)
-                                    .addOnSuccessListener(aVoid -> Toast.makeText(this, "Reset link sent to email", Toast.LENGTH_SHORT).show());
-                        }
-                    }
-                });
-                AlertDialog alert = alertDialog.create();
-                alert.show();
-            });
-            registration.setOnClickListener(v -> {
-                startActivity(new Intent(LoginActivity.this, Registration.class));
-                finish();
-            });
-//        }
+            AlertDialog alert = alertDialog.create();
+            alert.show();
+        });
+        registration.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, Registration.class));
+            finish();
+        });
 
     }
 }
